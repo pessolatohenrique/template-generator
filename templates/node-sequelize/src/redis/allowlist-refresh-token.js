10cadcdab5aac1a92eb3683f81ee7cdbb54a6f73
pessolatohenrique <pessolatohenrique@gmail.com>
2021-09-15 20:26:04 -0300

    Configuracao do Docker para template sequelize, incluindo redis

--- templates/node-sequelize/src/redis/allowlist-refresh-token.js
+++ templates/node-sequelize/src/redis/allowlist-refresh-token.js
@@ -1,4 +1,8 @@
 const redis = require("redis");
 const manipulateList = require("./manipulate-list");
-const allowList = redis.createClient({ prefix: "allowlist-refresh-token:" });
+const allowList = redis.createClient({
+  host: "redis",
+  port: 6379,
+  prefix: "allowlist-refresh-token:",
+});
 module.exports = manipulateList(allowList);
--- templates/node-sequelize/src/redis/forgotlist-token.js
+++ templates/node-sequelize/src/redis/forgotlist-token.js
@@ -1,4 +1,8 @@
 const redis = require("redis");
 const manipulateList = require("./manipulate-list");
-const forgotList = redis.createClient({ prefix: "forgotlist-token:" });
+const forgotList = redis.createClient({
+  host: "redis",
+  port: 6379,
+  prefix: "forgotlist-token:",
+});
 module.exports = manipulateList(forgotList);
