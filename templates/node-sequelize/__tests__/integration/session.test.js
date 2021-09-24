54d15430f036994ea380de4c6d92be612a462256
pessolatohenrique <pessolatohenrique@gmail.com>
2021-09-24 19:41:34 -0300

    Testes com geracao de token e refresh token

--- templates/node-sequelize/__tests__/integration/session.test.js
+++ templates/node-sequelize/__tests__/integration/session.test.js
@@ -8,13 +8,25 @@ describe("Authentication", () => {
       password: "admin@123",
     });
 
+    const responseRefresh = await request(app).post("/refresh_token").send({
+      refreshToken: response.body.refreshToken,
+    });
+
+    const responseRefreshInvalid = await request(app)
+      .post("/refresh_token")
+      .send({
+        refreshToken: 123213,
+      });
+
     expect(response.status).toBe(200);
+    expect(responseRefresh.status).toBe(200);
+    expect(responseRefreshInvalid.status).toBe(401);
   });
 
   it("should not authenticate with invalid credentials", async () => {
     const response = await request(app).post("/login").send({
       username: "pessolatohenrique",
-      password: "admin",
+      password: "fakepassword",
     });
 
     expect(response.status).toBe(401);
