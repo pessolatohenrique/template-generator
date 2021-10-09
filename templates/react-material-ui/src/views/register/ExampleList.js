7f30adabe681d25ef6461c9a6a44d87ccb847728
pessolatohenrique <pessolatohenrique@gmail.com>
2021-10-09 16:04:25 -0300

    react-material-ui: componente de listagem em tabela

--- templates/react-material-ui/src/views/register/ExampleList.js
+++ templates/react-material-ui/src/views/register/ExampleList.js
@@ -1,5 +1,92 @@
+import React from "react";
+import {
+  Container,
+  Card,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@mui/material";
+import { StyledTableCell } from "../../constants/CustomStyles";
+import { THEME_COLOR } from "../../constants/default_settings";
+import BreadcrumbsWrapper from "../../components/BreadcrumbsWrapper";
+
+function createData(name, author, pages, genre) {
+  return { name, author, pages, genre };
+}
+
+const rows = [
+  createData("Jogos Vorazes", "Suzanne Collins", 320, "Trilogia"),
+  createData(
+    "Harry Potter e a Pedra Filosofal",
+    "J.K Rowling",
+    220,
+    "Trilogia"
+  ),
+  createData("Me poupe!", "Nathalia Arcuri", 200, "Finanças"),
+  createData("Sherlock Homes", "Arthur Conan Doyle", 500, "Suspense"),
+  createData(
+    "O mistério dos sete relógios",
+    "Agatha Christie",
+    300,
+    "Suspense"
+  ),
+];
+
 function ExampleList() {
-  return <h1>Example List: {process.env.REACT_APP_API_URL}</h1>;
+  return (
+    <>
+      <Container fixed>
+        <br />
+        <Card>
+          <CardContent>
+            <BreadcrumbsWrapper childrenLabel="Livros" />
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="h1"
+              color={THEME_COLOR}
+            >
+              Livros
+            </Typography>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Título</StyledTableCell>
+                    <StyledTableCell>Autor</StyledTableCell>
+                    <StyledTableCell>Gênero</StyledTableCell>
+                    <StyledTableCell>Páginas</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => {
+                    return (
+                      <TableRow key={row.name}>
+                        <TableCell>{row.name}</TableCell>
+                        <TableCell>{row.author}</TableCell>
+                        <TableCell>{row.genre}</TableCell>
+                        <TableCell>{row.pages}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Adicionar</Button>
+          </CardActions>
+        </Card>
+      </Container>
+    </>
+  );
 }
 
 export default ExampleList;
