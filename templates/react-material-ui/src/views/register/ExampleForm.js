891ccd003683578b412d1eab0d4d056c0cd979ee
pessolatohenrique <pessolatohenrique@gmail.com>
2021-10-23 15:33:31 -0300

    react-material-ui: componentes de selectbox e selectbox dependente

--- templates/react-material-ui/src/views/register/ExampleForm.js
+++ templates/react-material-ui/src/views/register/ExampleForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Container,
   Card,
@@ -7,19 +7,49 @@ import {
   Grid,
   TextField,
   Button,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  FormHelperText,
+  FormGroup,
+  FormControlLabel,
+  Switch,
 } from "@mui/material";
 import { useForm } from "react-hook-form";
 
 import { THEME_COLOR } from "../../constants/default_settings";
 import BreadcrumbsWrapper from "../../components/BreadcrumbsWrapper";
 
+const books_data = [
+  { id: 5, name: "Harry Potter e a Pedra Filosofal", author: "jkrowling" },
+  { id: 7, name: "Harry Potter e a Câmera Secreta", author: "jkrowling" },
+  {
+    id: 9,
+    name: "Harry Potter e o Prisioneiro de Azkaban",
+    author: "jkrowling",
+  },
+  { id: 11, name: "Por que fazemos o que fazemos", author: "cortella" },
+  { id: 15, name: "Qual é a sua obra?", author: "cortella" },
+  { id: 18, name: "Me poupe!", author: "natharcuri" },
+];
+
 function ExampleForm() {
+  const [books, setBooks] = useState([]);
+
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
+
+  useEffect(() => {
+    const author = watch("author");
+    const books = [...books_data].filter((item) => item.author === author);
+    setBooks(books);
+  }, [watch("author")]);
 
   return (
     <>
@@ -61,7 +91,7 @@ function ExampleForm() {
                   <TextField
                     fullWidth
                     id="lastName"
-                    label="Lastname Name"
+                    label="Last Name"
                     inputProps={{ "data-testid": "lastName" }}
                     error={Boolean(errors.lastName)}
                     helperText={
@@ -69,6 +99,70 @@ function ExampleForm() {
                     }
                     {...register("lastName", { required: true, maxLength: 20 })}
                   />
+                </Grid>
+                <Grid item>
+                  <FormControl fullWidth>
+                    <InputLabel id="author">Author</InputLabel>
+
+                    <Select
+                      labelId="author"
+                      id="demo-simple-select-helper"
+                      fullWidth
+                      inputProps={{ "data-testid": "author" }}
+                      error={Boolean(errors.author)}
+                      {...register("author", {
+                        required: true,
+                      })}
+                    >
+                      <MenuItem value={"jkrowling"}>JK Rowling</MenuItem>
+                      <MenuItem value={"cortella"}>Cortella</MenuItem>
+                      <MenuItem value={"natharcuri"}>Nathalia Arcuri</MenuItem>
+                    </Select>
+                    {errors.author && (
+                      <FormHelperText error>
+                        With label + helper text
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+                </Grid>
+                <Grid item>
+                  <FormControl fullWidth>
+                    <InputLabel id="book">Book</InputLabel>
+
+                    <Select
+                      labelId="book"
+                      id="book"
+                      fullWidth
+                      inputProps={{ "data-testid": "book" }}
+                      error={Boolean(errors.book)}
+                      {...register("book", {
+                        required: true,
+                      })}
+                    >
+                      {books.map((item) => {
+                        return (
+                          <MenuItem key={item.id} value={item.id}>
+                            {item.name}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                    {errors.book && (
+                      <FormHelperText error>
+                        With label + helper text
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+                </Grid>
+                <Grid item>
+                  <FormGroup>
+                    <FormControlLabel
+                      control={
+                        <Switch defaultChecked {...register("active")} />
+                      }
+                      label="Ativo?"
+                    />
+                  </FormGroup>
                 </Grid>
                 <Grid item>
                   <Button
