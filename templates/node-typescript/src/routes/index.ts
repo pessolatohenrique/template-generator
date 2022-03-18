f8f775af29283517dcd80073155fecf563b5887d
pessolatohenrique <pessolatohenrique@gmail.com>
2022-03-18 19:12:35 -0300

    Node TS - Configuracoes gerais

--- templates/node-typescript/src/routes/index.ts
+++ templates/node-typescript/src/routes/index.ts
@@ -1,22 +1,24 @@
+import { Request, Response, Router } from "express";
+
 const bodyParser = require("body-parser");
 
-module.exports = (app) => {
+module.exports = (app: Router) => {
   app.use(bodyParser.json());
 
-  app.get("/author", (req, res) => {
+  app.get("/author", (req: Request, res: Response) => {
     return res
       .status(200)
-      .json({ message: "everything is fine in GET request" });
+      .json({ message: "everything is fine in GET request!" });
   });
 
-  app.post("/author", function (req, res) {
+  app.post("/author", function (req: Request, res: Response) {
     const body = req.body;
     return res
       .status(200)
       .json({ message: "everything is fine in POST request!", body });
   });
 
-  app.put("/author/:id", function (req, res) {
+  app.put("/author/:id", function (req: Request, res: Response) {
     const body = req.body;
     const params = req.params;
     return res
@@ -24,7 +26,7 @@ module.exports = (app) => {
       .json({ message: "everything is fine in PUT request!", body, params });
   });
 
-  app.delete("/author/:id", function (req, res) {
+  app.delete("/author/:id", function (req: Request, res: Response) {
     const params = req.params;
 
     return res
