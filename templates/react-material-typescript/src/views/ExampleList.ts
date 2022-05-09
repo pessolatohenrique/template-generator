f13ad76106afa5d6c057ff7aa2c3cb611caf262e
pessolatohenrique <pessolatohenrique@gmail.com>
2022-05-09 19:11:11 -0300

    React TS: configuracao de .env

--- templates/react-material-typescript/src/views/ExampleList.ts
+++
@@ -1,2 +0,0 @@
-// Silence is golden
-export function ExampleList() {}
--- templates/react-material-typescript/src/views/register/ExampleList.tsx
+++ templates/react-material-typescript/src/views/register/ExampleList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ExampleList() {
-  return <h1>Example List</h1>;
+  return <h1>Example List: {process.env.REACT_APP_API_URL}</h1>;
 }
 
 export default ExampleList;
