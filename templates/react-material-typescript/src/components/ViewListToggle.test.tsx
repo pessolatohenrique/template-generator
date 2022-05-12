3724938fe076b3ff97adb3cc8c63ff2f358aa7a1
pessolatohenrique <pessolatohenrique@gmail.com>
2022-05-12 20:00:57 -0300

    React TS - testes com eventos do ViewListToggle

--- templates/react-material-typescript/src/components/ViewListToggle.test.tsx
+++ templates/react-material-typescript/src/components/ViewListToggle.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { renderHook } from "@testing-library/react-hooks";
 import ViewListToggle from "./ViewListToggle";
 import useView from "../hooks/useView";
@@ -18,5 +18,48 @@ describe("ViewListToggle", () => {
     );
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("should change style on table click", () => {
+    const { result } = renderHook(() => useView());
+    const [isTable, isList, switchFormat] = result.current;
+
+    render(
+      <ViewListToggle
+        isTable={isTable}
+        isList={isList}
+        switchFormat={switchFormat}
+      />
+    );
+
+    const tableBtn = screen.getByTestId("view-table");
+    fireEvent.click(tableBtn);
+
+    expect(screen.getByTestId("view-table")).toHaveClass(
+      "MuiButton-containedPrimary"
+    );
+  });
+
+  it("should change style on card click", () => {
+    const { result } = renderHook(() => useView());
+    const [isTable, isList, switchFormat] = result.current;
+
+    render(
+      <ViewListToggle
+        isTable={isTable}
+        isList={isList}
+        switchFormat={switchFormat}
+      />
+    );
+
+    const cardBtn = screen.getByTestId("view-card");
+
+    fireEvent.click(cardBtn);
+
+    waitFor(() => {
+      expect(screen.getByTestId("view-card")).toHaveClass(
+        "MuiButton-containedPrimary"
+      );
+    });
   });
 });
--- templates/react-material-typescript/src/components/ViewListToggle.tsx
+++ templates/react-material-typescript/src/components/ViewListToggle.tsx
@@ -9,12 +9,14 @@ function ViewListToggle({ isTable, isList, switchFormat }: IViewListToggle) {
     <Grid container justifyContent="flex-end" gap="10px">
       <Button
         variant={isTable() ? "contained" : "text"}
+        data-testid="view-table"
         onClick={() => switchFormat("table")}
       >
         <ListIcon />
       </Button>
       <Button
         variant={isList() ? "contained" : "text"}
+        data-testid="view-card"
         onClick={() => switchFormat("list")}
       >
         <ViewModuleIcon />
